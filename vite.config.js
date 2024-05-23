8caff9c93d96d9cd1bc1785d490acb7a3957b2f4
drDeepl <Vlimaster@yandex.ru>
2024-05-23 23:42:31 +0700

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'drDeepl.github.io',
+  base: 'rick-morty-characters.github.io',
   plugins: [vue()],
   resolve: {
     alias: {
